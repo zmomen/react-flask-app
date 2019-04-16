bb599030265f127203b960911fd0991cd6e64ed6
Zaid Al-Momen <zmomen@andrew.cmu.edu>
2019-04-15 20:41:52 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ tasks = [
     {
         'id': 1,
         'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 'Bread',
         'done': False
     },
     {
