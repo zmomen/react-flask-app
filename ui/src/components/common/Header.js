3babfe0569b586d9807878ad0f539414a8195fba
Zaid Al-Momen <zmomen@andrew.cmu.edu>
2019-04-20 22:58:14 -0500

    Added Header.js for navs

---
+++ ui/src/components/common/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "spectre.css";
+import PageNotFound from "../../PageNotFound";
+
+
+const Header = () => {
+  return (
+    <nav>
+      <NavLink to="/" exact>Home</NavLink>{" | "}
+      <NavLink to="/courses" exact>Courses</NavLink>{" | "}
+      <NavLink to="/about" exact>About</NavLink>
+    </nav>
+  );
+};
+
+export default Header;
