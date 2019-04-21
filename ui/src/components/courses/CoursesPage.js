099307a585bb967c7a42ca319ba69c4a1ae334de
Zaid Al-Momen <zmomen@andrew.cmu.edu>
2019-04-20 23:01:58 -0500

    Added new component Courses

---
+++ ui/src/components/courses/CoursesPage.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      course: {
+        title: ""
+      }
+    };
+    //this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = event => {
+      const course = { ...this.state.course, title: event.target.value};
+      this.setState({course });
+  };
+
+  handleSubmit = event => {
+      event.preventDefault();
+      alert(this.state.course.title);
+  }
+
+  render() {
+      return (
+    <form onSubmit={this.handleSubmit}>
+      <h2>Courses</h2>
+      <h3>Add Course</h3>
+      <input
+        type="text"
+        onChange={this.handleChange}
+        value={this.state.course.title}
+      />
+      <input type="submit" value="Save" />
+      </form>);
+  }
+}
+
+export default CoursesPage;
