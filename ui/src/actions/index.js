11e61199038ee8a2033bd011614c7a981e84ce7f
Zaid Al-Momen <zalmomen01@gmail.com>
2019-05-09 19:20:02 -0500

    branch changes for redux

--- ui/src/actions/index.js
+++ ui/src/actions/index.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default function getArticles(){
     console.log("function a");
-    // return axios.get("localhost:5000/api/articles")
+    return axios.get("localhost:5000/api/articles")
 };
--- ui/src/components/redux/actions/courseActions.js
+++ ui/src/components/redux/actions/courseActions.js
@@ -1,3 +1,12 @@
+import * as courseApi from "../";
+
 export function createCourse(course) {
   return { type: "CREATE_COURSE", course: course };
 }
+
+export const getArticles = () => {
+  console.warn("im here");
+  return {
+    type: "GET_ARTICLES",
+  }
+};
