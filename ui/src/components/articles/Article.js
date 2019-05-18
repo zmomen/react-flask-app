0c7aa5720b54f145d1a33694eda90af35ac35410
Zaid Al-Momen <zalmomen01@gmail.com>
2019-05-18 12:23:33 -0500

    Added Image URL. CSS and date format [#14]

--- ui/src/components/articles/Article.js
+++ ui/src/components/articles/Article.js
@@ -1,24 +1,22 @@
 import React from "react";
 import { dateFmt } from "../../utils/helpers";
+import "./Article.css";
 
 const Article = props => {
   return (
     <div className="card">
       <div className="card-header">
-        <div className="card-title h6">{props.data.title}</div>
+        <div className="card-title h4">{props.data.title}</div>
         <div>{dateFmt(props.data.published)}</div>
       </div>
       <div className="card-footer">
-        {props.data.description}
-        <div className="card-image">
-          <img
-            width="300"
-            height="300"
-            src={props.data.img}
-            className="img-responsive"
-            alt="img here"
-          />
-        </div>
+        <img
+          width="300"
+          src={props.data.img}
+          className="img-responsive"
+          alt="img here"
+        />
+        <p>{props.data.description}</p>
       </div>
     </div>
   );
